78cb52f870b4f488ccd35973379386f59b157507
Abolfazl <abolfazlm181920@gmail.com>
2022-03-08 00:22:44 +0330

    DONE

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import React from 'react'
+import ContactForm from './Form';
 
 const App = () => {
   return (
     <div className="App">
-      App
+      <form action="https://formspree.io/f/xnqwvlln" method="POST">
+        <label>
+            Your email:
+            <input type="email" name="email" />
+        </label>
+        <label>
+            Your message:
+            <textarea name="message"></textarea>
+        </label>
+        <button type="submit">Send</button>
+        </form>
     </div>
   );
 }
 
-export default App;
+export default App
